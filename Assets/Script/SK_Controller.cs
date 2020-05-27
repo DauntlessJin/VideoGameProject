662c2709cb2fb993c584740cfb0874ed48188fd9
DauntlessJin <65750230+DauntlessJin@github.com>
2020-05-27 12:32:21 +0800

    change some functions and animations

--- Assets/Script/SK_Controller.cs
+++ Assets/Script/SK_Controller.cs
@@ -9,14 +9,19 @@ public class SK_Controller : MonoBehaviour
     private Rigidbody rigid_skate;
 
     int IdleHash = Animator.StringToHash("Locomotion.Idle");
+    int PushHash = Animator.StringToHash("Locomotion.Push");
+    int RunHash = Animator.StringToHash("Locomotion.FC_Cycle");
     //int OlliesHash = Animator.StringToHash("Ollies_system");
-    
+
     public float MaxSpeed = 0.06f;
     public float speed_m = 0.1f;
     public float jumpspeed = 100;
     public float gravity = 20.0f;
     private Vector3 moveDirection = Vector3.zero;
     private float junk_speed = 0.0f;
+
+    public float TimeLeft_Push = 10.0f;
+    public float TimeLeft_Stop = 5.0f;
 
     public WheelCollider[] wheelcollider = new WheelCollider[4];
     public Transform[] tireMeshes = new Transform[4];
@@ -32,31 +37,77 @@ public class SK_Controller : MonoBehaviour
     void Update()
     {
         AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
-        
-        var TurningSpeed = Input.GetAxis("Horizontal");
+
+        //var TurningSpeed = Input.GetAxis("Horizontal");
         var Speed = Input.GetAxis("Vertical");
         var Ollies_X = Input.GetAxis("Jump");
-
         _animator.SetFloat("Speed", Speed);
-        _animator.SetFloat("TurningSpeed", TurningSpeed);
-
-        Vector3 movedir = (Vector3.forward * Speed) + (Vector3.right * TurningSpeed);
-
         junk_speed = MaxSpeed;
 
-        ////////// When player pressed/unpressed "S" ///////////////////
+        //------------- Turning Animation -------------//
+        var Turning_Speed = Input.GetAxis("Horizontal");
+        _animator.SetFloat("Turning_Speed", Turning_Speed);
+        Vector3 movedir = (Vector3.forward * Speed) + (Vector3.right * Turning_Speed);
+
+
+        //------------- Start Run & delay 20 second -------------//
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Start_Run();
+            TimeLeft_Push = 10.0f;
+        }
+        
+        TimeLeft_Push -= Time.deltaTime;
+        Debug.Log("Timer = " + TimeLeft_Push);
+
+        if (TimeLeft_Push < 0)
+        {
+                _animator.SetTrigger("Push");
+                TimeLeft_Push = 10.0f;
+        }
+
+        //------------- Stop Animation -------------//
         if (Speed == -1 || stateInfo.nameHash == IdleHash)
         {
             skate_tr.Translate(0, 0, 0);
-            Speed = -1;
-            Debug.Log("Speed= " + Speed);
+            TimeLeft_Stop -= Time.deltaTime;
+            if (TimeLeft_Stop < 0)
+            {
+                Debug.Log("Start Stop Animation");
+                _animator.SetTrigger("Stop");
+                TimeLeft_Stop = 5.0f;
+            }
         }
         else if (Speed > -1)
         {
             skate_tr.Translate(0, 0, junk_speed);
         }
 
-        ////////// Ollies System /////////////////
+        if (Input.GetKey(KeyCode.S))
+        {
+            skate_tr.Translate(0, 0, 0);
+            TimeLeft_Stop -= Time.deltaTime;
+            if (TimeLeft_Stop < 0)
+            {
+                Debug.Log("Start Stop Animation");
+                _animator.SetTrigger("Stop");
+                TimeLeft_Stop = 5.0f;
+            }
+        }
+
+        //if (Input.GetKey(KeyCode.S) || stateInfo.nameHash == IdleHash)
+        //{
+        //    skate_tr.Translate(0, 0, 0);
+        //    TimeLeft_Stop -= Time.deltaTime;
+        //    if (TimeLeft_Stop < 0)
+        //    {
+        //        _animator.SetTrigger("Stop");
+        //        TimeLeft_Stop = 5.0f;
+        //    }
+        //}
+
+
+        //------------- Ollies System -------------//
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigid_skate.AddForce(Vector3.up * jumpspeed);
@@ -90,11 +141,17 @@ public class SK_Controller : MonoBehaviour
 
 
         /////////// Calculate Velocity ////////////
-        
-        
+
+
 
     }
     
+    void Start_Run()
+    {
+        _animator.SetTrigger("Start_Run");
+    }
+
+
     void UpdateMeshesPosition()
     {
         for(int i = 0; i < 4; i++)
